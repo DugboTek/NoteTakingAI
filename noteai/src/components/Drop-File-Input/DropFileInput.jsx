8c94b704bd1905b9554648027e55cd3de2c8966d
SDugbo <sola@dugbotek.com>
2023-04-08 20:04:36 -0400

    split tokens not workign file upload
    non working auido files, added code to count tokens and split recording

--- noteai/src/components/Drop-File-Input/DropFileInput.jsx
+++ noteai/src/components/Drop-File-Input/DropFileInput.jsx
@@ -5,41 +5,177 @@ import './drop-file-input.css';
 
 import { ImageConfig } from '../../config/imageConfig';
 import uploadImg from '../../assets/images/upload.png';
+import AudioBufferSlice from 'audiobuffer-slice';
+import decodeAudio, { decoders } from 'audio-decode';
+
 
 const DropFileInput = (props) => {
   const wrapperRef = useRef(null);
   const [fileList, setFileList] = useState([]);
   const [hasFile, setHasFile] = useState(false);
   const [formData, setFormData] = useState(null);
+  const [formDataList, setFormDataList] = useState([]);
+
 
   const onDrageEnter = () => wrapperRef.current.classList.add('dragover');
   const onDrageLeave = () => wrapperRef.current.classList.remove('dragover');
   const onDrop = () => wrapperRef.current.classList.remove('dragover');
 
-  const handleFile = async (e) => {
-    if (e.target.files && e.target.files[0]) {
-      const file = e.target.files[0];
-      const updatedList = [...fileList, file];
-      setFileList(updatedList);
-     // props.onFileChange(updatedList);
-      const data = new FormData();
-      data.append('file', file);
-      data.append('model', 'whisper-1');
-      setFormData(data);
-      console.log('File Uploaded- its me im handling the file');
-      props.setFormData(data);
-      //onFormDataChange(data);
+  // Checks if size of file is less than 25MB
+      // if (file.size > 25 * 1024 * 1024) {
+      //   alert('Please upload an audio file less than 25MB');
+      //   console.log('Please upload an audio file less than 25MB');
+      //   setHasFile(false); // reset the flag if the file is too big
+      //   return;
+      // }
+  
 
-      setHasFile(true); // set the flag to true
-      if (file.size > 25 * 1024 * 1024) {
-        alert('Please upload an audio file less than 25MB');
-        console.log('Please upload an audio file less than 25MB');
-        setHasFile(false); // reset the flag if the file is too big
-        return;
+      const handleFile = async (e) => {
+        if (e.target.files && e.target.files[0]) {
+          const file = e.target.files[0];
+          const updatedList = [...fileList, file];
+          setFileList(updatedList);
+      
+          try {
+            const arrayBuffer = await readFile(file);
+            const audioBuffer = await decodeAudio(arrayBuffer, file);
+      
+            // Modified sliceAudioBuffer usage
+            const chunks = [];
+            sliceAudioBuffer(audioBuffer, 24, async (slicedAudioBuffer) => {
+              chunks.push(slicedAudioBuffer);
+      
+              const chunkArrayBuffers = await Promise.all(chunks.map(audioBufferToArrayBuffer));
+      
+              let tempFormDataList = [];
+      
+              for (let i = 0; i < chunkArrayBuffers.length; i++) {
+                const chunkArrayBuffer = chunkArrayBuffers[i];
+                const fileExtension = file.type.split("/")[1];
+                const chunkFile = new File([chunkArrayBuffer], `audio_chunk_${i + 1}.${fileExtension}`, { type: file.type });
+      
+                console.log(`Chunk ${i + 1} - Type: ${chunkFile.type}, Name: ${chunkFile.name}`);
+      
+                const data = new FormData();
+                data.append('file', chunkFile, `audio_chunk_${i + 1}.${fileExtension}`);
+                data.append('model', 'whisper-1');
+                tempFormDataList.push(data);
+              }
+      
+              setFormDataList(tempFormDataList);
+              props.setFormDataList(tempFormDataList);
+            });
+      
+          } catch (error) {
+            console.error('Error processing audio file:', error);
+          }
+      
+          setHasFile(true);
+        }
+      };
+      
+      
+      
+      function base64ToBlob(base64Data, mimeType) {
+        const byteCharacters = atob(base64Data);
+        const byteNumbers = new Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+          byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        return new Blob([byteArray], { type: mimeType });
       }
-    }
-  };
+      
+      function createChunkFile(chunk, fileExtension, mimeType, i) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(chunk);
+          reader.onload = (event) => {
+            const chunkDataUrl = event.target.result;
+            const chunkBlob = base64ToBlob(chunkDataUrl.split(",")[1], mimeType);
+            const chunkFile = new File([chunkBlob], `audio_chunk_${i+1}.${fileExtension}`, { type: mimeType });
+            resolve(chunkFile);
+          };
+          reader.onerror = (error) => {
+            reject(error);
+          };
+        });
+      }
 
+      function readFile(file) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = () => resolve(reader.result);
+          reader.onerror = reject;
+          reader.readAsArrayBuffer(file);
+        });
+      }
+      
+      async function decodeAudio(arrayBuffer, file) {
+        const fileExtension = file.type.split("/")[1];
+        switch (fileExtension) {
+          case "mp3":
+            await decoders.mp3();
+            return decoders.mp3(arrayBuffer);
+          case "ogg":
+            await decoders.ogg();
+            return decoders.ogg(arrayBuffer);
+          case "wav":
+            await decoders.wav();
+            return decoders.wav(arrayBuffer);
+          case "flac":
+            await decoders.flac();
+            return decoders.flac(arrayBuffer);
+          case "opus":
+            await decoders.opus();
+            return decoders.opus(arrayBuffer);
+          case "qoa":
+            await decoders.qoa();
+            return decoders.qoa(arrayBuffer);
+          case "m4a":
+            await decoders.m4a();
+            return decoders.m4a(arrayBuffer);
+          default:
+            throw new Error(`Unsupported file format: ${fileExtension}`);
+        }
+      }
+      
+      
+      function sliceAudioBuffer(audioBuffer, chunkSizeInSeconds, callback) {
+        const chunkSizeInSamples = chunkSizeInSeconds * audioBuffer.sampleRate;
+        
+        for (let i = 0; i < audioBuffer.length; i += chunkSizeInSamples) {
+          const begin = i;
+          const end = Math.min(i + chunkSizeInSamples, audioBuffer.length);
+      
+          // Check if begin is within bounds of the audio buffer
+          if (begin < audioBuffer.length) {
+            AudioBufferSlice(audioBuffer, begin, end, (error, slicedAudioBuffer) => {
+              if (error) {
+                console.error(error);
+                return;
+              }
+              callback(slicedAudioBuffer);
+            });
+          }
+        }
+      }
+      
+      
+      
+      
+      
+      async function audioBufferToArrayBuffer(audioBuffer) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          const blob = new Blob([audioBuffer.getChannelData(0).buffer], { type: 'audio/wav' });
+          reader.onload = () => resolve(reader.result);
+          reader.onerror = reject;
+          reader.readAsArrayBuffer(blob);
+        });
+      }
+      
+      
   const fileRemove = (index) => {
     const updatedList = [...fileList];
     updatedList.splice(index, 1);
--- noteai/src/components/UploadFileBox.js
+++ noteai/src/components/UploadFileBox.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import DropFileInput from './Drop-File-Input/DropFileInput';
-import recordIcon from '../assets/images/record-black.png';
-import { WebVoiceProcessor } from '@picovoice/web-voice-processor';
-import lamejs from 'lamejstmp';
+import React, { useState, useEffect } from "react";
+import DropFileInput from "./Drop-File-Input/DropFileInput";
+import recordIcon from "../assets/images/record-black.png";
+import { WebVoiceProcessor } from "@picovoice/web-voice-processor";
+import lamejs from "lamejstmp";
+import GPT3Tokenizer from "gpt3-tokenizer";
 
-const OpenAI = require('openai');
-const { Configuration, OpenAIApi } = OpenAI;
+const OpenAI = require("openai");
 
 const key = process.env.React_App_OPEN_AI_API_KEY;
-console.log(key);
 
 const UploadFileBox = (props) => {
   const [convertedText, setConvertedText] = useState("");
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState(null);
+  const [formDataList, setFormDataList] = useState([]);
   const [hasFile, setHasFile] = useState(false);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [recording, setRecording] = useState(false);
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState("");
   const [old, setOld] = useState([]);
   const [userSubject, setUserSubject] = useState("");
   const [file, setFile] = useState(null);
@@ -25,55 +25,73 @@ const UploadFileBox = (props) => {
   const trackLengthInMS = 1000; // Length of audio chunk in miliseconds
   const maxNumOfSecs = 1000; // Number of mili seconds we support per recording (1 second)
 
-  const api_key = process.env.OPEN_AI_API_KEY;
+  const maxFileSize = 2 * 1024 * 1024; // 20 MB
+  let mp3Encoder;
 
-  const blobToFile = (theBlob, fileName) => {
-    const b = theBlob;
-    // Add properties to the blob
-    b.lastModifiedDate = new Date();
-    b.name = fileName;
-    const fileObj = new File([theBlob], fileName, { type: theBlob.type });
-    return fileObj;
+  function numTokensFromMessages(messages, model = "gpt3") {
+    const tokenizer = new GPT3Tokenizer({ type: model });
+    let numTokens = 0;
+    const encoded = tokenizer.encode(messages);
+    numTokens += encoded.bpe.length;
+    return numTokens;
   }
 
-  const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+  function splitTranscription(text, maxTokens = 3096) {
+	console.log("splitting text");
+    const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
+    const tokens = tokenizer.encode(text).bpe;
+    const segments = [];
 
-  const maxFileSize = 24 * 1024 * 1024; // 25 MB
-let mp3Encoder;
+    for (let i = 0; i < tokens.length; i += maxTokens) {
+      const segmentTokens = tokens.slice(i, i + maxTokens);
+      const segmentText = tokenizer.decode(segmentTokens);
+      segments.push(segmentText);
+    }
 
-const webVoiceProcessor = new WebVoiceProcessor({
-	context: "browsers",
-	frameLength: 512,
-	sampleRate: 16000
+    return segments;
+  }
+
+  const webVoiceProcessor = new WebVoiceProcessor({
+    context: "browsers",
+    frameLength: 512,
+    sampleRate: 16000,
   });
 
-const processAudioData = async (inputFrame) => {
-	const mp3Data = mp3Encoder.encodeBuffer(inputFrame);
-	if (mp3Data.length > 0) {
-		setRecordedChunks(prevChunks => [...prevChunks, mp3Data]);
-	}
-	let filesize = recordedChunks.length * Uint8Array.BYTES_PER_ELEMENT;
-	if(filesize %5 == 0){
-		console.log(filesize);
-	}
-  
-	if (recordedChunks.length * Uint8Array.BYTES_PER_ELEMENT > maxFileSize) {
-		console.log("Max file size reached. Stopping recording.");
-	  // Stop the recording
-	  await WebVoiceProcessor.unsubscribe(audioProcessor);
-	  // Send the audio and reset recordedChunks
-	  await sendAudio();
-	  setRecordedChunks([]);
-	  // Start a new recording session
-	  if (recording) {
-		console.log("restarting recording...");
-		await WebVoiceProcessor.subscribe(audioProcessor);
-	  }
-	}
+  const calculateFileSize = () => {
+	let fileSize = 0;
+	recordedChunks.forEach((chunk) => {
+	  fileSize += chunk.length;
+	});
+	return fileSize;
   };
-  
 
-const audioProcessor = {
+  const processAudioData = async (inputFrame) => {
+    const mp3Data = mp3Encoder.encodeBuffer(inputFrame);
+    if (mp3Data.length > 0) {
+      setRecordedChunks((prevChunks) => [...prevChunks, mp3Data]);
+    }
+    let filesize = calculateFileSize();
+    // if (filesize % 5 == 0) {
+    //   console.log(filesize);
+    // }
+
+    if (calculateFileSize() > maxFileSize) {
+      console.log("Max file size reached. Stopping recording. File size:");
+	  console.log(filesize);
+      // Stop the recording
+      await WebVoiceProcessor.unsubscribe(audioProcessor);
+      // Send the audio and reset recordedChunks
+      await sendAudio();
+      setRecordedChunks([]);
+      // Start a new recording session
+      if (recording) {
+        console.log("restarting recording...");
+        await WebVoiceProcessor.subscribe(audioProcessor);
+      }
+    }
+  };
+
+  const audioProcessor = {
     onmessage: (e) => {
       processAudioData(e.data.inputFrame);
     },
@@ -83,15 +101,15 @@ const audioProcessor = {
     if (recording) {
       // Stop the recording and get the recorded audio Blob as an MP3 file
       try {
-		console.log('ubsubscribeing...');
+        console.log("ubsubscribeing...");
         await WebVoiceProcessor.unsubscribe(audioProcessor);
-		console.log(recordedChunks);
+        console.log(recordedChunks);
         sendAudio();
       } catch (error) {
         console.error("An error occurred while stopping the recorder: ", error);
       } finally {
         setRecording(false);
-		console.log('stopping recording...');
+        console.log("stopping recording...");
       }
     } else {
       setRecording(true);
@@ -109,113 +127,185 @@ const audioProcessor = {
       }
     }
   };
+
+  const handleSubmit = async (inputText) => {
+    //gets the subject from the user
+    const resp = await fetch("https://scribb.ai:3001/noteDetails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userSubject }),
+    });
+
+    const response = await fetch("https://scribb.ai:3001", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ inputText }),
+    });
+    const data = await response.json(); //hi+old
+    const newMessage = data.message;
+    setOld((prevMessages) => [...prevMessages, newMessage]);
+    const allMessages = old.concat(newMessage).join("\n");
+
+    // Return the response message
+    return newMessage;
+  };
+
+  const sendAudio = async () => {
+    try {
+      props.isLoading(true);
+      setLoading(true);
+      // Stop recording and get the recorded audio Blob as an MP3 file
+      const audioBlob = new Blob(recordedChunks, { type: "audio/mpeg" });
+      let file = new File([audioBlob], "audio.mpeg");
+      // Create a FormData instance to send the file and model information
+      let data = new FormData();
+      data.append("file", file);
+      data.append("model", "whisper-1");
+      // Send the audio file to the API for transcription
+      let res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+        headers: {
+          Authorization: `Bearer ${key}`,
+        },
+        method: "POST",
+        body: data,
+      });
+      let responseJson = await res.json();
+      console.log(responseJson);
+
+      // Split the transcribed text into segments of 3096 tokens each
+      const textSegments = splitTranscription(responseJson.text);
+      let allResponses = [];
+
+      // Process each text segment separately
+      for (const textSegment of textSegments) {
+		console.log("processing segment");
+        const segmentResponse = await handleSubmit(textSegment);
+        allResponses.push(segmentResponse);
+      }
+
+      // Combine the responses
+      const combinedResponse = allResponses.join("\n");
+      setConvertedText(combinedResponse);
+      props.onConvertedText(combinedResponse);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+      props.isLoading(false);
+    }
+  };
+
+  const sendAudioFile = async (dataList) => {
+	try {
+	  props.isLoading(true);
+	  setLoading(true);
   
-	const handleSubmit = async (inputText) => {
-		//gets the subject from the user
-		const resp = await fetch('https://scribb.ai:3001/noteDetails', {
-			method : 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({userSubject}),
-		});
-
-		const response = await fetch('https://scribb.ai:3001', {
-		  method: 'POST',
-		  headers: {
-			'Content-Type': 'application/json'
-		  },
-		  body: JSON.stringify({inputText}),
-		})
-		const data = await response.json();//hi+old
-		const newMessage = data.message;
-		setOld((prevMessages) => [...prevMessages, newMessage]);
-		const allMessages = old.concat(newMessage).join("\n");
-		setConvertedText(data.message);
-	//	props.setConvertedText(data.message);
-		props.onConvertedText(allMessages);
-		//setOld(convertedText);
-		setLoading(false);
-		props.isLoading(false);
-		console.log(data.message);
-		/*.then(res => res.json())
-		.then((data) => setResponse(data.message))
-		.then((data) => setConvertedText(data.message))
-		.then((data) => props.onConvertedText(data.message));*/
-		console.log("submitted");
-
-	  };
-
+	  let allResponses = [];
+  
+	  for (let index = 0; index < dataList.length; index++) {
+		const data = dataList[index];
+  
+		const chunkFile = data.get('file');
+		console.log(`Sending chunk ${index + 1} - Type: ${chunkFile.type}, Name: ${chunkFile.name}`); // Log the chunk information
+  
+		// Send the audio file to the API for transcription
+		let res = await Promise.race([
+			fetch("https://api.openai.com/v1/audio/transcriptions", {
+			  headers: {
+				Authorization: `Bearer ${key}`,
+			  },
+			  method: "POST",
+			  body: data,
+			}),
+			new Promise((_, reject) =>
+			  setTimeout(() => reject(`Timeout on chunk ${index + 1}`), 1280000)
+			),
+		  ]);
 	
-	
+  
+		let responseJson = await res.json();
+		console.log(responseJson);
+  
+		// Split the transcribed text into segments of 3096 tokens each
+		//const textSegments = splitTranscription(responseJson.text);
+  
+		// Process each text segment separately
+		/*for (const textSegment of textSegments) {
+		  console.log("processing segment");
+		  const segmentResponse = await handleSubmit(textSegment);
+		  allResponses.push(segmentResponse);
+		}*/
+		const respseg = await handleSubmit(responseJson.text);
+		allResponses.push(respseg);
+	  }
+  
+	  // Combine the responses
+	  const combinedResponse = allResponses.join("\n");
+	  setConvertedText(combinedResponse);
+	  props.onConvertedText(combinedResponse);
+  
+	} catch (error) {
+	  console.error(error);
+	} finally {
+	  setLoading(false);
+	  props.isLoading(false);
+	}
+  };
+  
+  
+  
 
-	  const sendAudio = async () => {
-		try {
-		  props.isLoading(true);
-		  setLoading(true);
-		  // Stop recording and get the recorded audio Blob as an MP3 file
-		  const audioBlob = new Blob(recordedChunks, { type: 'audio/mpeg' });
-		  let file = new File([audioBlob], 'audio.mpeg');
-		  // Create a FormData instance to send the file and model information
-		  let data = new FormData();
-		  data.append('file', file);
-		  data.append('model', 'whisper-1');
-		  // Send the audio file to the API for transcription
-		  let res = await fetch('https://api.openai.com/v1/audio/transcriptions', {
-			headers: {
-			  Authorization: `Bearer ${key}`,
-			},
-			method: 'POST',
-			body: data,
-		  });
-		  let responseJson = await res.json();
-		  console.log(responseJson);
-		  // Set the converted text and user subject in the parent component
-		 // setConvertedText(responseJson.text);
-		 // props.onConvertedText(responseJson.text);
-		 // setUserSubject('Computer Networks');
-		  console.log('userSubject in send audio');
-		  console.log(userSubject);
-		  // Handle the form submit with the converted text
-		  handleSubmit(responseJson.text);
-		} catch (error) {
-		  console.error(error);
-		} finally {
-		  setLoading(false);
-		  props.isLoading(false);
-		}
-	  };
-	  
-	
+  return (
+    <div className="box--upload">
+      <div className="microphone-box">
+        <button
+          className={`record-button${recording ? "-recording" : ""}`}
+          onClick={toggleAudioRecording}
+        >
+          <img
+            src={recordIcon}
+            className="record-icon"
+            alt="recording button"
+          />
+        </button>
+      </div>
+      <h2 className="header">Get Summerized Lectures With AI</h2>
 
-	return(
-		<div className="box--upload">
-			<div className ="microphone-box">
-				<button className={`record-button${recording ? '-recording' : ''}`} onClick={toggleAudioRecording}>
-				<img src={recordIcon} className="record-icon" alt= "recording button"/>
-				</button>
-			</div>
-			<h2 className="header">
-				Get Summerized Lectures With AI
-			</h2>
+      {
+        <DropFileInput setFormDataList={setFormDataList} />
+        //setFormData={setFormData}
+        //onFileChange={handleFile}
+      }
+      <form>
+        <div class="input-container">
+          <input
+            type="text"
+            onChange={(e) => setUserSubject(e.target.value)}
+            id="subject"
+            name="subject"
+            placeholder="Enter The Class Subject"
+            required
+            class="text-input"
+          />
+          <label for="subject" class="label">
+            Subject
+          </label>
+        </div>
 
-			{
-			<DropFileInput setFormData={setFormData}/>
-				//setFormData={setFormData}
-				//onFileChange={handleFile}
-
-
-			}
-			<form> 
-				<div class = "input-container">
-					<input type="text" onChange ={(e) => setUserSubject(e.target.value)} id="subject" name="subject" placeholder="Enter The Class Subject" required class = "text-input"/>
-					<label for="subject" class = "label">Subject</label>
-				</div>
-			
-			<button type = "button" className ="ant-btn padded marginTop" onClick={sendAudio}>Generate Notes</button>
-			</form>
-		</div>
-	)
+        <button
+          type="button"
+          className="ant-btn padded marginTop"
+          onClick={() => sendAudioFile(formDataList)}
+        >
+          Generate Notes
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default UploadFileBox;
