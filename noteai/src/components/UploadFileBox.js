c2dec9d2d01523dba9921d3f08b406a32aee4617
SDugbo <sola@dugbotek.com>
2023-04-09 18:32:16 -0400

    Fixed Recording feature
    added better longer recording
    removed herobrine

--- noteai/src/components/UploadFileBox.js
+++ noteai/src/components/UploadFileBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import DropFileInput from './Drop-File-Input/DropFileInput';
 import recordIcon from '../assets/images/record-black.png';
 import { WebVoiceProcessor } from '@picovoice/web-voice-processor';
@@ -8,7 +8,6 @@ const OpenAI = require('openai');
 const { Configuration, OpenAIApi } = OpenAI;
 
 const key = process.env.React_App_OPEN_AI_API_KEY;
-console.log(key);
 
 const UploadFileBox = (props) => {
   const [convertedText, setConvertedText] = useState("");
@@ -21,13 +20,21 @@ const UploadFileBox = (props) => {
   const [old, setOld] = useState([]);
   const [userSubject, setUserSubject] = useState("");
   const [file, setFile] = useState(null);
-  const [recordedChunks, setRecordedChunks] = useState([]);
-  const [shouldProcessAudio, setShouldProcessAudio] = useState(true);
 
-  const trackLengthInMS = 1000; // Length of audio chunk in miliseconds
+  const [elapsedRecordingTime, setElapsedRecordingTime] = useState(0);
+
+  const [autoResumeRecording, setAutoResumeRecording] = useState(false);
+
+  const recordedChunksRef = useRef([]);
+  const recordedChunksRefOld = useRef([]);
+
+
+
+  const trackLengthInMS = (512 / 16000) * 1000; // Length of audio chunk in miliseconds
   const maxNumOfSecs = 1000; // Number of mili seconds we support per recording (1 second)
 
   const api_key = process.env.OPEN_AI_API_KEY;
+  const TIMEOUT_MS = 1000 * 60 * 1; // 5 minutes
 
   const blobToFile = (theBlob, fileName) => {
     const b = theBlob;
@@ -43,33 +50,99 @@ const UploadFileBox = (props) => {
   const maxFileSize = 1 * 1024 * 1024; // 25 MB
 let mp3Encoder;
 
+// useEffect(() => {
+// 	let timer;
+  
+// 	if (recording) {
+// 	  timer = setTimeout(async () => {
+// 		setAutoResumeRecording(true);
+// 		await toggleAudioRecording(); // Toggle off
+// 		setTimeout(toggleAudioRecording, TIMEOUT_MS); // Schedule toggling on after 5 minutes
+// 	  }, TIMEOUT_MS); // Schedule toggling off after 5 minutes
+// 	}
+  
+// 	return () => {
+// 	  if (timer) clearTimeout(timer);
+// 	};
+//   }, [recording]);
+
+// useEffect(()  => {
+// 	if (elapsedRecordingTime >= TIMEOUT_MS) {
+// 		setElapsedRecordingTime(0);
+// 		restartAudio(); 
+// 	  }
+//   }, [elapsedRecordingTime]);
+
+const intervalRef = useRef(null);
+
+useEffect(() => {
+  if (recording) {
+	if (intervalRef.current) clearInterval(intervalRef.current);
+
+	intervalRef.current = setInterval(async () => {
+	  setAutoResumeRecording(true);
+	  await restartAudio(); // Restart the recording
+	}, TIMEOUT_MS); // Schedule restarting every 1 minute
+  } else {
+	if (intervalRef.current) clearInterval(intervalRef.current);
+  }
+
+  return () => {
+	if (intervalRef.current) clearInterval(intervalRef.current);
+  };
+}, [recording]);
+
+
+
+  const restartAudio = async () => {
+	console.log("Timeout reached. Stopping recording.");
+	// Stop the recording
+	await WebVoiceProcessor.unsubscribe(audioProcessor);
+	recordedChunksRefOld.current = recordedChunksRef.current;
+	recordedChunksRef.current = [];
+	 // Reset elapsedRecordingTime
+	 // Start a new recording session
+	 if (recording) {
+	   console.log("restarting recording...");
+	   mp3Encoder = new lamejs.Mp3Encoder(1, 16000, 128);
+  
+	   // Create a new WebVoiceProcessor instance
+	   const webVoiceProcessor = new WebVoiceProcessor({
+		 context: "browsers",
+		 frameLength: 512,
+		 sampleRate: 16000,
+	   });
+   
+	   // Start recording using the Web Voice Processor
+	   try {
+		 console.log("Recording Audio...");
+		 await WebVoiceProcessor.subscribe(audioProcessor);
+	   } catch (error) {
+		 console.error("An error occurred while starting the recorder: ", error);
+		 setRecording(false);
+	   }
+	 }
+	// Send the audio and reset recordedChunks
+	await sendAudio();
+  }
+
+  
+
 const processAudioData = async (inputFrame) => {
 	// If audio processing is disabled, return early
-	if (!shouldProcessAudio) return;
   
 	const mp3Data = mp3Encoder.encodeBuffer(inputFrame);
 	if (mp3Data.length > 0) {
-	  setRecordedChunks(prevChunks => [...prevChunks, mp3Data]);
+	  recordedChunksRef.current = [...recordedChunksRef.current, mp3Data];
 	}
-	let filesize = recordedChunks.length * Uint8Array.BYTES_PER_ELEMENT;
-	if(filesize %5 == 0){
-	  //console.log(filesize);
-	}
+	let filesize = recordedChunksRef.current.length * Uint8Array.BYTES_PER_ELEMENT;
   
-	if (recordedChunks.length * Uint8Array.BYTES_PER_ELEMENT > maxFileSize) {
-	  console.log("Max file size reached. Stopping recording.");
-	  // Stop the recording
-	  await WebVoiceProcessor.unsubscribe(audioProcessor);
-	  // Send the audio and reset recordedChunks
-	  await sendAudio();
-	  setRecordedChunks([]);
-	  // Start a new recording session
-	  if (recording) {
-		console.log("restarting recording...");
-		await WebVoiceProcessor.subscribe(audioProcessor);
-	  }
-	}
+	// Update elapsedRecordingTime
+	setElapsedRecordingTime(prevTime => prevTime + trackLengthInMS);
+	
   };
+  
+  
   
 
 const audioProcessor = {
@@ -83,21 +156,20 @@ const audioProcessor = {
 	  // Stop the recording and get the recorded audio Blob as an MP3 file
 	  try {
 		console.log('unsubscribing...');
-		setShouldProcessAudio(false); 
+		console.log(recordedChunksRef);
 		await WebVoiceProcessor.unsubscribe(audioProcessor);
 		await WebVoiceProcessor.reset();
-		console.log(recordedChunks);
-		await sendAudio();
-		
+		recordedChunksRefOld.current = recordedChunksRef.current;
+		sendAudio().finally(() => {
+		  setRecording(false);
+		  console.log('stopping recording...');
+		  recordedChunksRef.current = [];
+		});
 	  } catch (error) {
 		console.error("An error occurred while stopping the recorder: ", error);
-	  } finally {
-		setRecording(false);
-		console.log('stopping recording...');
 	  }
 	} else {
 	  setRecording(true);
-	  setShouldProcessAudio(true); 
   
 	  // Initialize the MP3 encoder
 	  mp3Encoder = new lamejs.Mp3Encoder(1, 16000, 128);
@@ -106,7 +178,7 @@ const audioProcessor = {
 	  const webVoiceProcessor = new WebVoiceProcessor({
 		context: "browsers",
 		frameLength: 512,
-		sampleRate: 16000
+		sampleRate: 16000,
 	  });
   
 	  // Start recording using the Web Voice Processor
@@ -119,81 +191,75 @@ const audioProcessor = {
 	  }
 	}
   };
-	const handleSubmit = async (inputText) => {
-		//gets the subject from the user
-		const resp = await fetch('https://scribb.ai:3001/noteDetails', {
-			method : 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({userSubject}),
-		});
-
-		const response = await fetch('https://scribb.ai:3001', {
-		  method: 'POST',
-		  headers: {
-			'Content-Type': 'application/json'
-		  },
-		  body: JSON.stringify({inputText}),
-		})
-		const data = await response.json();//hi+old
-		const newMessage = data.message;
-		setOld((prevMessages) => [...prevMessages, newMessage]);
-		const allMessages = old.concat(newMessage).join("\n");
-		setConvertedText(data.message);
-	//	props.setConvertedText(data.message);
-		props.onConvertedText(allMessages);
-		//setOld(convertedText);
-		setLoading(false);
-		props.isLoading(false);
-		console.log(data.message);
-		/*.then(res => res.json())
-		.then((data) => setResponse(data.message))
-		.then((data) => setConvertedText(data.message))
-		.then((data) => props.onConvertedText(data.message));*/
-		console.log("submitted");
-
-	  };
+  
+  const handleSubmit = async (inputText) => {
+	//gets the subject from the user
+	const resp = await fetch('https://scribb.ai:3001/noteDetails', {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({ userSubject }),
+	});
+  
+	const response = await fetch('https://scribb.ai:3001', {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({ inputText }),
+	});
+	const data = await response.json();
+	const newMessage = data.message;
+  
+	setOld((prevMessages) => {
+	  const allMessages = [...prevMessages, newMessage].join("\n");
+	  props.onConvertedText(allMessages);
+	  return [...prevMessages, newMessage];
+	});
+  
+	setLoading(false);
+	props.isLoading(false);
+	console.log(data.message);
+	console.log("submitted");
+  };
+  
 
 	
 	
 
 	  const sendAudio = async () => {
+		
 		try {
 		  props.isLoading(true);
 		  setLoading(true);
 		  // Stop recording and get the recorded audio Blob as an MP3 file
-		  const audioBlob = new Blob(recordedChunks, { type: 'audio/mpeg' });
-		  let file = new File([audioBlob], 'audio.mpeg');
+		  const audioBlob = new Blob(recordedChunksRefOld.current, { type: "audio/mpeg" });
+		  let file = new File([audioBlob], "audio.mpeg");
 		  // Create a FormData instance to send the file and model information
 		  let data = new FormData();
-		  data.append('file', file);
-		  data.append('model', 'whisper-1');
+		  data.append("file", file);
+		  data.append("model", "whisper-1");
 		  // Send the audio file to the API for transcription
-		  let res = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+		  let res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
 			headers: {
 			  Authorization: `Bearer ${key}`,
 			},
-			method: 'POST',
+			method: "POST",
 			body: data,
 		  });
 		  let responseJson = await res.json();
 		  console.log(responseJson);
-		  // Set the converted text and user subject in the parent component
-		 // setConvertedText(responseJson.text);
-		 // props.onConvertedText(responseJson.text);
-		 // setUserSubject('Computer Networks');
-		  console.log('userSubject in send audio');
-		  console.log(userSubject);
 		  // Handle the form submit with the converted text
 		  handleSubmit(responseJson.text);
 		} catch (error) {
 		  console.error(error);
 		} finally {
 		  // Reset the recordedChunks array
-		  setRecordedChunks([]);
+		  recordedChunksRefOld.current = [];
 		}
 	  };
+	  
 	  
 	
 
@@ -204,6 +270,7 @@ const audioProcessor = {
 				<button className={`record-button${recording ? '-recording' : ''}`} onClick={toggleAudioRecording}>
 				<img src={recordIcon} className="record-icon" alt= "recording button"/>
 				</button>
+				<div className="indicator"></div>
 			</div>
 			<div className="instructions">
 				<b>Press the button</b>
@@ -214,7 +281,7 @@ const audioProcessor = {
 			</div>
 			<div class="tab-container">
 					<div class="tab">
-						<span>Record Audio</span>
+						<span>Record</span>
 					</div>
 					<div class="tab-upload">
 						<span>Upload</span>
