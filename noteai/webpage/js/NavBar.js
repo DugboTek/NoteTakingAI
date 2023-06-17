e1bfa2c073813423eedf3d009e49f4890f3428db
SDugbo <sola@dugbotek.com>
2023-06-17 18:06:19 -0400

    NEW homepage

---
+++ noteai/webpage/js/NavBar.js
@@ -0,0 +1,65 @@
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('.navbar__logo');
+//const mainContent = document.querySelector('.main__content');
+//const stickyBar = document.querySelector('.sticky-bar')
+let lastScroll = 0;
+
+
+
+// Display Mobile 
+const mobileMenu = () =>{
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu); 
+
+const highlightMenu = ()=> {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
+
+    // adds the 'highlight' class to the menu itesm
+    if(window.innerWidth > 960 && scrollPos < 600){
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return
+    }
+
+    else if(window.innerWidth > 960 && scrollPos < 1388){
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return
+    }
+    else if(window.innerWidth > 960 && scrollPos < 2345){
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        homeMenu.classList.remove('highlight');
+        return
+    }
+
+    if((elem && window.innerWidth<960 && scrollPos <600) || elm){
+        elem.classList.remove('highlight');
+    } 
+}
+
+window.addEventListener('scroll',highlightMenu);
+//window.addEventListener('scroll', Consultation);
+window.addEventListener('click',highlightMenu);
+
+//close mobile menu
+const hideMobileMenu = ()=>{
+    const menuBars = document.querySelector('is-active');
+    if(window.innerWidth <= 768 && menuBars);
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+}
+
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
